a351fe8575d547d7b1566eba37a98bf8c3543513
njgeorge000158 <137228821+njgeorge000158@users.noreply.github.com>
2023-07-30 13:16:58 -0700

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,15 +3,16 @@
  #  File Name:  main.py
  #
  #  File Description:
- #      This program reads budget data from a csv file in the Resources folder, 
- #      budget_data.csv, composed of two columns: "Date" and "Profit/Losses".  
- #      The Python script analyzes the records to calculate each of the following 
- #      values: the total number of months included in the dataset, the net total 
- #      amount of "Profit/Losses" over the entire period, the changes in "Profit/Losses" 
- #      over the entire period and the average of those changes, the greatest increase 
- #      in profits (date and amount) over the entire period, and the greatest decrease 
- #      in profits (date and amount) over the entire period.  In addition, the program 
- #      both prints the analysis to the terminal and exports a text file with the results.
+ #      This program, main.py, reads budget data from a csv file in the Resources folder, 
+ #      budget_data.csv, composed of two columns: "Date" and "Profit/Losses".  The Python 
+ #      script analyzes the records to calculate each of the following values: the total 
+ #      number of months included in the dataset, the net total amount of "Profit/Losses" 
+ #      over the entire period, the changes in "Profit/Losses" over the entire period and 
+ #      the average of those changes, the greatest increase in profits (date and amount) 
+ #      over the entire period, and the greatest decrease in profits (date and amount) over 
+ #      the entire period.  In addition, the program both prints the analysis to the terminal 
+ #      and exports a text file with the results, budget_data_analysis.txt, to the analysis 
+ #      folder.
  #
  #
  #  Date            Description                             Programmer
